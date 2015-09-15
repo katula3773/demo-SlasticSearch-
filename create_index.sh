3ce73761c2b543492cc11e652eda7112bd2680e4
Phongnv <phongv3773@gmail.com>
2015-09-15 16:20:21 +0700

    new demo slasticSearch

---
+++ create_index.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# đâu là dữ liệu mẫu tùy vào bảng và các colum khac nhau ta co the tùy biến json theo yêu cầu
+
+# 
+
+curl -XPUT "http://localhost:9200/demo/color/1" -d '
+{
+		color: "red keya keyb keyc biet",
+		value: "#f00"
+	}'
+
+curl -XPUT "http://localhost:9200/demo/color/2" -d '
+{
+		color: "green keya ",
+		value: "#0f0"
+	}'
+
+curl -XPUT "http://localhost:9200/demo/color/3" -d '
+{
+		color: "blue keya keyb biet",
+		value: "#00f"
+	}'
+
+curl -XPUT "http://localhost:9200/demo/color/4" -d '
+{
+		color: "cyan keya",
+		value: "#0ff"
+	}'
+
+curl -XPUT "http://localhost:9200/demo/color/5" -d '
+{
+		color: "magenta keyc ",
+		value: "#f0f"
+	}'
+
+curl -XPUT "http://localhost:9200/demo/color/6" -d '
+{
+		color: "yellow keyb keyc ",
+		value: "#ff0"
+	}'
+
+curl -XPUT "http://localhost:9200/demo/color/7" -d '
+{
+		color: "black keyb keyc ",
+		value: "#000"
+	}'
+
+
+
---
+++ curlSearch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# search tất cả ở các doccument
+
+curl -XPOST "http://localhost:9200/_search" -d'
+{
+    "query": {
+        "query_string": {
+            "query": "keya"
+        }
+    }
+}'
+
+
+# Search sâu vào từng khu vực doccument 
+# curl -XPOST "http://localhost:9200/movies/_search" -d'
+# {
+#     "query": {
+#         "query_string": {
+#             "query": "biet"
+#         }
+#     }
+# }'
